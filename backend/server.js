68bda97755d53a220c274dca4a8efe3ed40ebccc
MohAli92 <148862863+MohAli92@users.noreply.github.com>
2025-01-18 21:58:00 +0100

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -6,7 +6,6 @@ const multer = require('multer');
 const cors = require('cors'); // لتفعيل CORS
 const WebSocket = require('ws');
 
-// إعداد Multer لتخزين الملفات المرفوعة في المجلد data مع أسماء فريدة
 const storage = multer.diskStorage({
     destination: (req, file, cb) => {
         cb(null, path.join(__dirname, '..', 'data')); // تخزين الملفات في data
@@ -21,18 +20,14 @@ const upload = multer({ storage });
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// تفعيل CORS للسماح بالطلبات من الفرونت إند
 app.use(cors());
 
-// تقديم ملفات الفرونت إند
 const frontendPath = path.join(__dirname, '..', 'frontend');
 app.use(express.static(frontendPath));
 
-// استخدام راوت observations.js
 const observationsRoutes = require('../routes/observations');
 app.use('/observations', observationsRoutes);
 
-// إعداد WebSocket
 const wss = new WebSocket.Server({ noServer: true });
 
 wss.on('connection', (ws) => {
@@ -68,14 +63,11 @@ wss.on('connection', (ws) => {
     });
 });
 
-// API لرفع الملفات ومعالجتها
 app.post('/upload', upload.single('file'), (req, res) => {
     const filePath = req.file.path; // مسار الملف المرفوع
 
-    // مسار سكريبت Python
     const scriptPath = path.join(__dirname, '..', 'scripts', 'process_ecg.py');
 
-    // إعداد الأمر لتشغيل السكريبت مع تغليف المسارات
     const command = `python "${scriptPath}" "${filePath}"`;
 
     exec(command, { cwd: path.join(__dirname, '..') }, (error, stdout, stderr) => {
@@ -86,7 +78,6 @@ app.post('/upload', upload.single('file'), (req, res) => {
 
         console.log('Python script output:', stdout);
 
-        // مسار ملف JSON الناتج
         const outputFilePath = path.join(__dirname, '..', 'output', 'fhir_observations.json');
         fs.readFile(outputFilePath, 'utf8', (err, data) => {
             if (err) {
@@ -98,12 +89,10 @@ app.post('/upload', upload.single('file'), (req, res) => {
     });
 });
 
-// تشغيل الخادم
 const server = app.listen(PORT, () => {
     console.log(`Server running on http://localhost:${PORT}`);
 });
 
-// إعداد WebSocket عند ترقية الطلبات
 server.on('upgrade', (req, socket, head) => {
     wss.handleUpgrade(req, socket, head, (ws) => {
         wss.emit('connection', ws, req);
